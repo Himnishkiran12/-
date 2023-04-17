8715f7f52f5a70531ab31c682047bfddf24c3a44
Himnishkiran12 <79916257+Himnishkiran12@users.noreply.github.com>
2023-04-17 22:18:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(800,450);
+canvas.center();
+}
+
+function draw(){
+
+}
+function take_snap_of_clown(){
+    save("clown.png")
+}
